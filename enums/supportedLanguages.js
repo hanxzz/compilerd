3a0d3361f5d557f769f733db09844345260ec7df
Han <43496337+hanxzz@users.noreply.github.com>
2024-07-01 02:54:21 +0530

    Update supportedLanguages.js

--- enums/supportedLanguages.js
+++ enums/supportedLanguages.js
@@ -1,12 +1,12 @@
-module.exports = {
-    C: 'c',
-    CPP: 'cpp',
-    PYTHON: 'python',
-    JAVA: 'java',
-    NODEJS: 'nodejs',
-    RUBY: 'ruby',
-    PROMPTV1: 'promptv1',
+@@ -9,8 +9,8 @@ module.exports = {
     PROMPTV2: 'promptv2',
     MULTIFILE: 'multifile',
     SQLITE3: 'sqlite3',
+    GO, // Added Go 
+    PHP, // Added Php 
+    RUST, // Added rust 
+    GO: 'go', // Added Golang 
+    PHP: 'php', // Added Php 
+    RUST: 'rust', // Added Rust 
+
 }
